c0b5c5c8af43a30d04a2d801c9d584bc4a8a62d8
User <user@somemailadress.com>
2023-02-05 18:27:25 +0100

    init simple ci version scripts

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,11 +1,21 @@
 // used Gradle 7.6
+import java.io.File
+import java.io.FileInputStream
+import java.io.FileOutputStream
+import java.util.*
 
 plugins {
     id("java")
 }
 
+val versionPropPath = "version.properties"
+val versionProp = initVersionProp()
+version = versionProp.getProperty("version")
+
+var tmpArtifactName = "HelloWorld-Gradle-$version.jar"
+var artifactName = "HelloWorld-$version.jar"
+
 group = "testpackage.test"
-version = "1.0-SNAPSHOT"
 var mainClass = "testpackage.test.App"
 
 repositories {
@@ -32,5 +42,45 @@ tasks.withType<Jar>() {
     duplicatesStrategy = DuplicatesStrategy.EXCLUDE
     configurations["compileClasspath"].forEach { file: File ->
         from(zipTree(file.absoluteFile))
+    }
+    // rename Artifact
+    doLast {
+        project.exec {
+            val currDirecory = System.getProperty("user.dir")
+            commandLine("bash", "-c", "cd $currDirecory/build/libs/ && mv $tmpArtifactName $artifactName")
+        }
+    }
+}
+
+// load Version
+fun initVersionProp():Properties {
+    return Properties().apply {
+        load(FileInputStream(versionPropPath))
+    }
+}
+
+// set Version
+tasks.register("setVersion") {
+    val version = System.getProperty("version")
+    versionProp.setProperty("version", version)
+    versionProp.store(FileOutputStream(versionPropPath), null)
+}
+
+// get Version
+tasks.register("getVersion"){
+    println(version)
+}
+
+// get Version, but cut off Snapshot (Ex.: 1.1.5-SNAPSHOT - > 1.1.5)
+tasks.register("getVersionWithoutSnapshot"){
+    project.exec {
+        commandLine("bash", "-c", "echo $version | sed -r 's/([^-]*)-.*/\\1/g'")
+    }
+}
+
+// get Version, but cut off Patch-Version (Ex.: 0.2.1 -> 0.2)
+tasks.register("getReleaseBranchName"){
+    project.exec {
+        commandLine("bash", "-c", "echo $version | sed -r 's/([^\\.]*\\.[^\\.]*)\\..*/\\1/g'")
     }
 }
